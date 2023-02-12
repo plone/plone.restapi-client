f82ad437c3ef6e2a88b0edf39354c1c50b6ac0f6
Víctor Fernández de Alba <sneridagh@gmail.com>
2023-02-12 18:39:05 +0100

    Prepare for release as a library (#1)

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { createContentQuery } from './content/add';
+import { getContentQuery } from './content/get';
+import { updateContentQuery } from './content/update';
+import { deleteContentQuery } from './content/delete';
+import { loginQuery } from './login/post';
+
+export {
+  createContentQuery,
+  getContentQuery,
+  updateContentQuery,
+  loginQuery,
+  deleteContentQuery,
+};
