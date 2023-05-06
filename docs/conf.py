b861084e59a80cae4c9f49ce89980056d1a5e661
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-05-06 11:01:54 +0200

    Last amendments

--- docs/conf.py
+++ docs/conf.py
@@ -213,12 +213,6 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
 }
 
-
-# -- GraphViz configuration ----------------------------------
-
-# graphviz_output_format = "svg"
-
-
 # -- OpenGraph configuration ----------------------------------
 
 ogp_site_url = "https://6.docs.plone.org/"
@@ -237,22 +231,6 @@ copybutton_prompt_is_regexp = True
 
 # -- sphinx.ext.todo -----------------------
 todo_include_todos = True  # Uncomment to show todos.
-
-
-# An extension that allows replacements for code blocks that
-# are not supported in `rst_epilog` or other substitutions.
-# https://stackoverflow.com/a/56328457/2214933
-# def source_replace(app, docname, source):
-#     result = source[0]
-#     for key in app.config.source_replacements:
-#         result = result.replace(key, app.config.source_replacements[key])
-#     source[0] = result
-
-
-# # Dict of replacements.
-# source_replacements = {
-#     "{PLONE_BACKEND_VERSION}": "6.0.4",
-# }
 
 
 def setup(app):
