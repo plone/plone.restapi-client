ee0de23f3a46615431b5b12d60aa4f69deb0c974
hemant-hc <iamhemantchaudhary1@gmail.com>
2023-07-12 18:06:43 +0530

    action for group root

---
+++ src/restapi/groups/get_root.ts
@@ -0,0 +1,23 @@
+import { handleRequest, ApiRequestParams } from '../../API';
+import { PloneClientConfig } from '../../interfaces/config';
+import { GetGroupRootResponse } from '../../interfaces/groups';
+
+export type GroupsRootArgs = {
+  config: PloneClientConfig;
+};
+
+export const getGroupsRoot = async ({
+  config,
+}: GroupsRootArgs): Promise<GetGroupRootResponse> => {
+  const options: ApiRequestParams = {
+    config,
+    params: {},
+  };
+
+  return handleRequest('get', '/@groups', options);
+};
+
+export const getGroupsRootQuery = ({ config }: GroupsRootArgs) => ({
+  queryKey: ['get', 'groups'],
+  queryFn: () => getGroupsRoot({ config }),
+});
