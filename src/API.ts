d21ecc3a19d83dcb9d1eeaac3d8d0d0216ba02bc
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-07-21 23:43:16 +0200

    blueprint of the axios refactor

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,4 @@
-import superagent from 'superagent';
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import Cookies from 'universal-cookie';
 import type request from 'superagent';
 import { PloneClientConfig } from './interfaces/config';
@@ -12,6 +12,7 @@ export type ApiRequestParams = {
   type?: any;
   headers?: any;
   checkUrl?: boolean;
+  raw?: boolean;
 };
 
 function getBackendURL(apiPath: string, path: string) {
@@ -24,103 +25,66 @@ function getBackendURL(apiPath: string, path: string) {
   return `${apiPath}${APISUFIX}${adjustedPath}`;
 }
 
-export async function handleRequest(
+// export async function handleRequest(
+//   method: string,
+//   path: string,
+//   options: ApiRequestParams,
+// ): Promise<any> {
+//   const fetcher = api;
+//   const response = await fetcher[method](path, options);
+
+//   return response;
+// }
+
+const _handleResponse = ({ data }: AxiosResponse) => data;
+
+const _handleError = (error: any) => Promise.reject(error);
+
+function axiosConfigAdapter(
+  method: string,
+  path: string,
+  options: ApiRequestParams,
+): AxiosRequestConfig {
+  const {
+    config,
+    params,
+    data,
+    type,
+    headers = {},
+    checkUrl = false,
+  }: ApiRequestParams = options;
+  const axiosConfig: AxiosRequestConfig = {
+    method,
+    url: getBackendURL(config.apiPath, path),
+    params,
+    headers: {
+      Accept: 'application/json',
+      ...headers,
+    },
+    data,
+    validateStatus: function (status) {
+      return status >= 200 && status < 300; // default
+    },
+  };
+  console.log(axiosConfig);
+  if (config.token) {
+    if (axiosConfig.headers) {
+      axiosConfig.headers['Authorization'] = `Bearer ${config.token}`;
+    }
+  }
+
+  return axiosConfig;
+}
+
+export async function apiRequest(
   method: string,
   path: string,
   options: ApiRequestParams,
 ): Promise<any> {
-  const fetcher = new API();
-  const response = await fetcher[method](path, options);
-
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
+  if (options.raw) {
+    axios.interceptors.response.use(null, _handleError);
+  } else {
+    axios.interceptors.response.use(_handleResponse, _handleError);
   }
-  return response.body;
-}
-
-export default class API {
-  [m: string]: (
-    path: string,
-    options: ApiRequestParams,
-  ) => Promise<request.Response>;
-
-  constructor(req?: Request) {
-    const cookies = new Cookies();
-
-    methods.forEach((method) => {
-      this[method] = async (
-        path,
-        {
-          config,
-          params,
-          data,
-          type,
-          headers = {},
-          checkUrl = false,
-        }: ApiRequestParams,
-      ) => {
-        let request: request.SuperAgentRequest;
-
-        // @ts-ignore
-        request = superagent[method](getBackendURL(config.apiPath, path));
-
-        if (params) {
-          request.query(params);
-        }
-        // let authToken;
-        // if (req) {
-        //   // @ts-ignore
-        //   // We are in SSR
-        //   authToken = req.universalCookies.get('auth_token');
-        //   request.use(addHeadersFactory(req));
-        // } else {
-        //   authToken = cookies.get('auth_token');
-        // }
-
-        if (config.token) {
-          request.set('Authorization', `Bearer ${config.token}`);
-        }
-
-        request.set('Accept', 'application/json');
-
-        if (type) {
-          request.type(type);
-        }
-
-        Object.keys(headers).forEach((key) => request.set(key, headers[key]));
-
-        if (data) {
-          request.send(data);
-        }
-
-        return request;
-
-        // request.end((err, response) => {
-        //   if (
-        //     checkUrl &&
-        //     request.url &&
-        //     request.xhr &&
-        //     stripQuerystring(request.url) !==
-        //       stripQuerystring(request.xhr.responseURL)
-        //   ) {
-        //     if (request.xhr.responseURL?.length === 0) {
-        //       return reject({
-        //         code: 408,
-        //         status: 408,
-        //         url: request.xhr.responseURL,
-        //       });
-        //     }
-        //     return reject({
-        //       code: 301,
-        //       url: request.xhr.responseURL,
-        //     });
-        //   }
-        //   return err ? reject(err) : resolve(response.body || response.text);
-        // });
-
-        // promise.request = request;
-        // return promise;
-      };
-    });
-  }
+  return axios(axiosConfigAdapter(method, path, options));
 }
