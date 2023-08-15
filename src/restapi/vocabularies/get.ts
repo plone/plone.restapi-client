7773aab228475cff82989710265536fc787093a7
hemant-hc <iamhemantchaudhary1@gmail.com>
2023-08-15 17:08:52 +0530

    filters for vocabularies

--- src/restapi/vocabularies/get.ts
+++ src/restapi/vocabularies/get.ts
@@ -5,6 +5,9 @@ import { z } from 'zod';
 
 const getVocabulariesSchema = z.object({
   path: z.string(),
+  title: z.string().optional(),
+  token: z.string().optional(),
+  tokens: z.array(z.string()).optional(),
 });
 
 export type VocabulariesArgs = z.infer<typeof getVocabulariesSchema> & {
@@ -13,22 +16,38 @@ export type VocabulariesArgs = z.infer<typeof getVocabulariesSchema> & {
 
 export const getVocabularies = async ({
   path,
+  title,
+  token,
+  tokens,
   config,
 }: VocabulariesArgs): Promise<GetVocabulariesResponse> => {
   const validatedArgs = getVocabulariesSchema.parse({
     path,
+    title,
+    token,
+    tokens,
   });
 
   const options: ApiRequestParams = {
     config,
-    params: {},
+    params: {
+      ...(validatedArgs.title && { title: validatedArgs.title }),
+      ...(validatedArgs.token && { token: validatedArgs.token }),
+      ...(validatedArgs.tokens && { tokens: validatedArgs.tokens }),
+    },
   };
   const vocabulariesPath = `@vocabularies/${validatedArgs.path}`;
 
   return apiRequest('get', vocabulariesPath, options);
 };
 
-export const getVocabulariesQuery = ({ path, config }: VocabulariesArgs) => ({
-  queryKey: [path, 'get', 'vocabularies'],
-  queryFn: () => getVocabularies({ path, config }),
+export const getVocabulariesQuery = ({
+  path,
+  title,
+  token,
+  tokens,
+  config,
+}: VocabulariesArgs) => ({
+  queryKey: [path, title, token, tokens, 'get', 'vocabularies'],
+  queryFn: () => getVocabularies({ path, title, token, tokens, config }),
 });
