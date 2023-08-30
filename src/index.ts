34d2c1a8626f95e86bdac4d837aeb69a38a657dc
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-08-30 16:36:46 +0200

    Fix build, add new externals, fix imports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-import { getContentQuery } from './restapi/content/get';
-import { createContentMutation } from './restapi/content/add';
-import { updateContentMutation } from './restapi/content/update';
-import { deleteContentMutation } from './restapi/content/delete';
-import { loginQuery } from './restapi/login/post';
 import ploneClient from './client';
-
-export {
-  createContentMutation,
-  getContentQuery,
-  updateContentMutation,
-  loginQuery,
-  deleteContentMutation,
-};
 
 export default ploneClient;
