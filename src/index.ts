da61d4796509b631aeb8dc9beff9e53ad8153b41
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-10-11 20:44:43 +0200

    Working

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import ploneClient from './client';
-import { PloneClientProvider, usePloneClient } from './clientProvider';
 
-export { PloneClientProvider, usePloneClient };
 export default ploneClient;
